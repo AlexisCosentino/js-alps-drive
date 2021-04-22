77a9d9faefd3d3d09d4178d4728770e66808c85a
Alexis Cosentino <alexis27_08@hotmail.fr>
2021-04-22 09:22:04 +0200

    I can put a new file in drive, all http error are good

--- drive.js
+++ drive.js
@@ -3,6 +3,7 @@ const path = require('path')
 const fs = require('fs/promises');
 const { Dirent } = require('fs');
 
+
 const ALPS_DRIVE = path.join(os.tmpdir(), 'test/');
 console.log('Mon dossier racine de stockage ; ' + ALPS_DRIVE);
 
@@ -27,23 +28,40 @@ function listFolder(path){
         if (err.code == 'ENOTDIR'){
             return fs.readFile(path);
         }
+        if (err.code == 'ENOENT'){
+            throw new Error('le dossier néxiste pas')
+        }
         })
 }
+
 
 function createFolder(dir, name){
     return fs.mkdir(path.join(dir, name)).then(()=>{
         console.log('Le dossier à bien été créé')
-    }).catch(()=>{
+    }).catch((err)=>{
         console.log('Votre dossier na pas "été créé')
+        if (err.code =='ENOENT'){
+            throw new Error('Le dossier néxiste pas')
+        }
     })
 }
 
 function deleteFolder(dir, name){
     return fs.rm(path.join(dir, name), {recursive: true}).then(()=> {
-        console.log('Votre dossier à bien été supprimé')
+        console.log('Votre dossier ou fichier à bien été supprimé')
+    }).catch((err) => {
+        console.log('Impossible de supprimer votrer dossier ou fichier...')
+        if (err.code == 'ENOENT'){
+            throw new Error('Le dossier néxiste pas')
+        }
+    })
+}
+
+function addFile(name, dir, src){
+    return fs.copyFile(src, path.join(dir, name)).then(() => {
+        console.log('Votre fichier à bien été envoyé sur le drive')
     }).catch(() => {
-        console.log(path)
-        console.log('Impossible de supprimer')
+        console.log('Votre fichier na pas été téléchargé...')
     })
 }
 
@@ -54,5 +72,6 @@ module.exports = {
     listFolder: listFolder,
     createFolder: createFolder,
     deleteFolder: deleteFolder,
+    addFile: addFile,
     ALPS_DRIVE: ALPS_DRIVE
 }
--- server.js
+++ server.js
@@ -1,9 +1,14 @@
-console.log('hello la famille je suis le server')
+console.log('hello la famille je suis le server');
 let express = require('express');
-const os = require('os')
+const os = require('os');
 const fs = require('fs');
+const path = require('path');
 const drive = require('./drive');
 const { fileURLToPath } = require('url');
+const isAlphanumeric = require('is-alphanumeric')
+const bb = require('express-busboy');
+
+
 
 let app = express();
 let port = 3000
@@ -19,69 +24,110 @@ function start() {
 }
 
 
-///////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 app.get('/api/drive', function (req, res) {
   drive.listFolder(drive.ALPS_DRIVE).then((result) => {
     res.send(result);
+  }).catch((error) => {
+    res.status(404).send(error.message);
   })
 })
 
-////////////////////////////////////////////////////// 
+////////////////////////////
 
 app.get('/api/drive/:name', function (req, res) {
   drive.listFolder(drive.ALPS_DRIVE + req.params.name).then((result) => {
-      if (fs.existsSync(drive.ALPS_DRIVE + req.params.name)) {
-        res.send(result);
-      } else {
-      res.status(404);
-    }
+    res.send(result);
+  }).catch((error) => {
+    res.status(404).send(error.message);
   })
 })
 
 
-///////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 //Pouvoir creer un nouveau dossier
 
 app.post('/api/drive', function (req, res) {
-  drive.createFolder(drive.ALPS_DRIVE, req.query.name).then((result) => {
-    res.send(result);
-  })
+  if (isAlphanumeric(req.query.name) == false) {
+    res.status(400).send('erreur ma gueule')
+  } else {
+    drive.createFolder(drive.ALPS_DRIVE, req.query.name).then((result) => {
+      res.status(201).send(result);
+    })
+  }
 })
 
 // Pouvoir creer un nouveau dossier dans les dossiers enfants
 
 app.post('/api/drive/:name', function (req, res) {
-  drive.createFolder(drive.ALPS_DRIVE + req.params.name, req.query.name).then((result) => {
-    res.send(result);
-  })
+  if (isAlphanumeric(req.query.name) == false) {
+    res.status(400).send('erreur ma gueule')
+  } else {
+    drive.createFolder(drive.ALPS_DRIVE + req.params.name, req.query.name).then((result) => {
+      res.status(201).send(result);
+    }).catch((error) => {
+      res.status(404).send(error.message);
+    })
+  }
 })
 
-//////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 //Pouvoir supprimer un dossier ou un fichier
 
 app.delete('/api/drive/:name', function (req, res) {
-  console.log('coucou chaton')
-  drive.deleteFolder(drive.ALPS_DRIVE, req.params.name).then ((result) => {
-    res.send(result);
-  })
+  if (isAlphanumeric(req.params.name) == false) {
+    res.status(400).send('erreur ma gueule')
+  } else {
+    drive.deleteFolder(drive.ALPS_DRIVE, req.params.name).then((result) => {
+      res.status(201).send(result);
+    }).catch((error) => {
+      res.status(404).send(error.message);
+    })
+  }
 })
 
 ////Pouvoir supprimer un dossier ou un fichier dans un dossier enfant
 
 app.delete('/api/drive/:name/:second', function (req, res) {
-  console.log('coucou chaton')
-  drive.deleteFolder(drive.ALPS_DRIVE + req.params.name, req.params.second).then ((result) => {
-    res.send(result);
+  if (isAlphanumeric(req.params.second) == false) {
+    res.status(400).send('erreur ma gueule')
+  } else {
+    drive.deleteFolder(drive.ALPS_DRIVE + req.params.name, req.params.second).then((result) => {
+      res.status(201).send(result);
+    }).catch((error) => {
+      res.status(404).send(error.message);
+    })
+  }
+})
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+///////////Pouvoir ajouter un fichier
+
+//J'utilise et initialise Busboy
+bb.extend(app, {
+  upload: true,
+  path: path.join(os.tmpdir(), 'busboy/'),
+});
+
+app.put('/api/drive', function (req, res) {
+  console.log(isAlphanumeric('text.txt'))
+  drive.addFile(req.files.file.filename, drive.ALPS_DRIVE, req.files.file.file).then((result) => {
+    res.status(201).send(result);
   })
 })
 
+app.put('/api/drive/:name', function (req, res) {
+  drive.addFile(req.files.filename, drive.ALPS_DRIVE + req.params.name, req.files.file.file).then((result) => {
+    res.status(201).send(result);
+  })
+})
 
-
-
+//////////////
 
 module.exports = {
-    start: start,
-  }
+  start: start,
+}
